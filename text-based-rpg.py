e044e364377c26c5051784051c2bc040d9aa8764
BorgorNinja <128947459+BorgorNinja@users.noreply.github.com>
2023-11-03 19:29:37 +0800

    Debugged the Code

--- text-based-rpg.py
+++ text-based-rpg.py
@@ -2,11 +2,6 @@ import os, random, time
 Sword = 3
 Staff = 1
 Stave = 2
-
-print("""
-a. Start Game
-b. Options
-c. Quit""")
 while True:
     try:
         option = None
@@ -16,13 +11,14 @@ while True:
 
                        a. Start Game
                        b. Settings
-                       c. Exit""")
-        if (option.lower == "a"):
+                       c. Exit \nAnswer: """)
+        os.system("cls")
+        if (option == "a"):
             print("The Game will start.")
             time.sleep(5)
             os.system("cls")
             game_session()
-        elif (option.lower == "b"):
+        elif (option == "b"):
             optionLoop = True
             TamperedWithSettings = True
             while (optionLoop == True):
@@ -31,27 +27,30 @@ while True:
                     b. Set Equipment (Sword, Staff, Stave)
                     c. Quit Back to Main Menu""")
                 optionSet = input("Option Select: ")
-                if (optionSet.lower == "a"):
+                if (optionSet == "a"):
                     optionDifficulty = input("Enter 1-3 number to set Difficulty: ")
                     print("Difficulty Set to ", optionDifficulty)
                     optionLoop = False
-                elif (optionSet.lower == "b"):
-                    while (optionLoop)
-                    optionWeapon = input("Select Starter Weapon (a. Sword \nb. Staff \nc. Stave)")
-                    if (optionWeapon.lower == "a"):
-                        print("Now will start with the weapon Sword.")
-                        optionWeaponFinal = Sword
-                    elif (optionWeapon.lower == "b"):
-                        print("Now will Start with the weapon Staff.")
-                        optionWeaponFinal = Staff
-                    elif (optionWeapon.lower == "c"):
-                        print("Now will Start with the weapon Stave.")
-                        optionWeaponFinal = Stave
-                    else:
-                        print("Invalid Weapon choice. Try Again")
-                elif (optionSet.lower == "c"):
+                elif (optionSet == "b"):
+                    while (optionLoop == True):
+                        optionWeapon = input("Select Starter Weapon (a. Sword \nb. Staff \nc. Stave)")
+                        if (optionWeapon == "a"):
+                            print("Now will start with the weapon Sword.")
+                            optionWeaponFinal = Sword
+                            optionLoop = False
+                        elif (optionWeapon == "b"):
+                            print("Now will Start with the weapon Staff.")
+                            optionWeaponFinal = Staff
+                            optionLoop = False
+                        elif (optionWeapon == "c"):
+                            print("Now will Start with the weapon Stave.")
+                            optionWeaponFinal = Stave
+                            optionLoop = False
+                        else:
+                            print("Invalid Weapon choice. Try Again")
+                elif (optionSet == "c"):
                     optionLoop = False
-        elif (option.lower == "c"):
+        elif (option == "c"):
             print("Quitting Program... \nThanks for playing.")
             time.sleep(5)
             break
@@ -63,6 +62,14 @@ while True:
         
 def game_session():
     character = []
-    enemies = []
+    enemies = ["Slime", "Goblin","Kobold","Dragon"]
+    enemySubclass = ["Great","Greater","Poisonous","Disaster","Calamity"]
     playerEquipment = [optionWeapon]
+    gameLoop = True
+    rolledEnemy = random.choice(enemies)
+    rolledSubclass = random.choice(enemySubclass)
+    rolledEnemyBoss = rolledEnemy0 + " " + rolledSubclass
+    #while (gameLoop = True):
+
+        
     #All the Game Code Goes here
